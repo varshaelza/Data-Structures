ecd64e088db8197b59b6d41a0d0b70c5b996ad7f
Jodek <devassijoseph.946@gmail.com>
2020-05-04 19:26:27 +0530

    Creating

---
+++ Documents/stack.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int arr[1000000],top=-1;
+void push(int d)
+{	arr[++top]=d;
+	
+	
+}
+void pop()
+{
+	
+		top--;
+
+}
+
+
+void main()
+{	char s[5];
+	int i,q,d,big,j;
+	scanf("%d",&q);
+	for(i=0;i<q;i++)
+	{
+		scanf("%s[^' ']",s,&d);
+		if(s[3]=='h')
+		{	scanf("%d",&d);
+			push(d);
+		}
+		else if(s[2]=='p')
+		{
+			pop();
+		}
+		else 
+		{
+			 big=arr[top];
+			for(j=top-1;j>-1;j--)
+			{
+			if(arr[j]>big)
+				big=arr[j];	
+			}
+			printf("%d\n",big);
+		}
+		
+	}
+	
+}
